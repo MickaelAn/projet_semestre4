c7b32e875c0699c8d15b0dc76235c97e29b31fd3
Mickael Ancel <mick.ancel@gmail.com>
2015-03-18 20:40:59 +0100

    Ajout de l'API de Twitter dans le site web

--- index.php
+++ index.php
@@ -24,7 +24,9 @@
 
     </div>
     <div class="tv-twitter">
-        
+                    <a class="twitter-timeline"  href="https://twitter.com/MMI_Troyes" data-widget-id="573774469891813376">Tweets de @MMI_Troyes</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)) {js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
     </div>
     <div class="tv-planning">
 
