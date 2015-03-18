5b397886732da0277ce9eb443c81305a50f495d9
Mickael Ancel <mick.ancel@gmail.com>
2015-03-18 17:35:03 +0100

    Première utilisation de Git

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>MMI TV</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+<body>
+    <div class="tv-barre">
+        <div class="tv-date">
+            Mercredi 18 février 2015
+        </div>
+        <div class="tv-heure">
+            11:30
+        </div>
+    </div>
+    <div class="tv-player">
+
+    </div>
+    <div class="tv-meteo">
+
+    </div>
+    <div class="tv-programme">
+
+    </div>
+    <div class="tv-twitter">
+
+    </div>
+    <div class="tv-planning">
+
+    </div>
+</body>
+</html>
